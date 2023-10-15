0fea886e41406e28158ca9939579738f82ef389e
Federico Balbuena <federico.balbuena@gmail.com>
2023-10-15 18:54:36 -0300

    Subo modulo ingreso ticker pw002

--- src/app/components/ingreso/ingreso.component.ts
+++ src/app/components/ingreso/ingreso.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class IngresoComponent {
 
+  ingresarCodigo() {
+    var ver = document.getElementById('inputCodigo') as HTMLElement;
+    ver.style.visibility = "visible";
+  }
 }
