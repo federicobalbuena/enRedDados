8194d21c291de14117f08211e592a888833ec6fd
federicobalbuena <92692343+federicobalbuena@users.noreply.github.com>
2023-10-20 09:48:47 -0300

    Se sube componente temporizador, se agregan casilleros al tablero y se eliminan los background-color del tablero y el temporizador, se modifica color de casilla amarilla para que coincida con el color del dado (#6)

--- src/app/components/temporizador/temporizador.component.ts
+++ src/app/components/temporizador/temporizador.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./temporizador.component.css']
 })
 export class TemporizadorComponent {
+  tiempoRestante: number = 15; // El valor lo va a tomar segun la dificultad seleccionada al momento de crear la partida
+  colorFondo: string = 'rgb(35, 177, 77)'
 
+  iniciarCuenta() {
+    let interval = setInterval(() => { // Crea un intervalo que se ejecute cada segundo
+
+      this.tiempoRestante > 0 ? this.cuentaAtras() : this.cambioJugador(interval);
+      
+    }, 1000); // El segundo parámetro es el tiempo en milisegundos entre cada ejecución
+  }
+
+  cuentaAtras() {
+    this.tiempoRestante--;
+    if (this.tiempoRestante < 5) {
+      this.colorFondo = 'rgb(254, 45, 57)';
+      
+    } else if (this.tiempoRestante < 10) {
+      this.colorFondo = 'rgb(255, 197, 35)';
+      
+    }
+  }
+
+  cambioJugador(interval: any) {
+    this.colorFondo = 'rgb(35, 177, 77)';
+    this.tiempoRestante = 15;
+    clearInterval(interval);
+  }
 }
