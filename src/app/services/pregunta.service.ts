84e541b6e5c287196d7168a0c116c912ac045bf4
Federico Balbuena <federico.balbuena@gmail.com>
2023-11-08 10:44:31 -0300

    Se sube logica del componente creacion-sala, logica del componente jugadores modo solitario, se modifica CSS del componente temporizador

--- src/app/services/pregunta.service.ts
+++ src/app/services/pregunta.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import  { ObjPregunta }  from '../models/objPregunta';
+import  { ObjPregunta }  from '../models/ObjPregunta';
 import { PreguntaResponse } from '../models/preguntaResponse';
+import { Sala } from '../models/sala';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,18 @@ export class PreguntaService {
   private preguntaResponseSubject = new BehaviorSubject<PreguntaResponse>(new PreguntaResponse());
   public preguntaResponse = this.preguntaResponseSubject.asObservable();
 
+  
+/*   public cantAvanzar$ = new BehaviorSubject<number>(0);
+  public cantRetroceder$ = new BehaviorSubject<number>(0);
+
+  public mostrarMensaje$ = new BehaviorSubject<boolean>(false); */
+
+  public sala$ = new BehaviorSubject<Sala>(new Sala());
+
+
   url = "http:///api/PreguntasCDM/";
   headers = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };
+ 
   
   constructor(private http: HttpClient) { }
 
