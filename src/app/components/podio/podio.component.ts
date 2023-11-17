716957ec07d52d5881d6aec3c3c3e5c955578331
Federico Balbuena <federico.balbuena@gmail.com>
2023-11-17 10:30:29 -0300

    Subo logica  del componente podio

--- src/app/components/podio/podio.component.ts
+++ src/app/components/podio/podio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,12 +6,21 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './podio.component.html',
   styleUrls: ['./podio.component.css']
 })
-export class PodioComponent {
+export class PodioComponent implements OnInit{
   constructor (private route: ActivatedRoute) {};
-
-  ganadores = JSON.parse(this.route.snapshot.queryParams['array']);
   
+  @ViewChild('audio')
+
+  audio!: ElementRef;
+  
+  ganadores = JSON.parse(this.route.snapshot.queryParams['array']);
   primero = this.ganadores[0].nombreJugador;
   segundo = this.ganadores[1].nombreJugador;
   tercero = this.ganadores[2].nombreJugador;
+
+  ngOnInit() {
+    
+  }
+  //this.audio.nativeElement.play(); Esto irá dentro del metodo que muestre los ganadores segun la cantidad de participantes
+
 }
